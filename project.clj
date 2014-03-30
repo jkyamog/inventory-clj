154c1a2329eafc523197480e057afab77237587b
Jun Yamog <jkyamog@gmail.com>
2014-03-30 15:41:00 +1300

    initial luminus project using profile +cljs +postgres

---
+++ project.clj
@@ -0,0 +1,62 @@
+(defproject
+  inventory
+  "0.1.0-SNAPSHOT"
+  :repl-options
+  {:init-ns inventory.repl}
+  :dependencies
+  [[ring-server "0.3.1"]
+   [domina "1.0.2"]
+   [ragtime "0.3.4"]
+   [environ "0.4.0"]
+   [markdown-clj "0.9.41"]
+   [com.taoensso/timbre "3.1.6"]
+   [prismatic/dommy "0.1.2"]
+   [korma "0.3.0-RC6"]
+   [org.clojure/clojurescript "0.0-2138"]
+   [org.clojure/clojure "1.6.0"]
+   [com.taoensso/tower "2.0.2"]
+   [log4j
+    "1.2.17"
+    :exclusions
+    [javax.mail/mail
+     javax.jms/jms
+     com.sun.jdmk/jmxtools
+     com.sun.jmx/jmxri]]
+   [selmer "0.6.4"]
+   [cljs-ajax "0.2.3"]
+   [lib-noir "0.8.1"]
+   [compojure "1.1.6"]
+   [postgresql/postgresql "9.1-901.jdbc4"]]
+  :cljsbuild
+  {:builds
+   [{:source-paths ["src-cljs"],
+     :compiler
+     {:pretty-print false,
+      :output-to "resources/public/js/site.js",
+      :optimizations :advanced}}]}
+  :ring
+  {:handler inventory.handler/app,
+   :init inventory.handler/init,
+   :destroy inventory.handler/destroy}
+  :ragtime
+  {:migrations ragtime.sql.files/migrations,
+   :database
+   "jdbc:postgresql://localhost/inventory?user=db_user_name_here&password=db_user_password_here"}
+  :profiles
+  {:uberjar {:aot :all},
+   :production
+   {:ring
+    {:open-browser? false, :stacktraces? false, :auto-reload? false}},
+   :dev
+   {:dependencies [[ring-mock "0.1.5"] [ring/ring-devel "1.2.2"]],
+    :env {:dev true}}}
+  :url
+  "http://example.com/FIXME"
+  :plugins
+  [[lein-ring "0.8.10"]
+   [lein-environ "0.4.0"]
+   [ragtime/ragtime.lein "0.3.4"]
+   [lein-cljsbuild "0.3.3"]]
+  :description
+  "FIXME: write description"
+  :min-lein-version "2.0.0")
---
+++ src/inventory/models/schema.clj
@@ -0,0 +1,9 @@
+(ns inventory.models.schema)
+
+(def db-spec
+  {:subprotocol "postgresql"
+   :subname "//localhost/inventory"
+   :user "db_user_name_here"
+   :password "db_user_password_here"})
+
+
